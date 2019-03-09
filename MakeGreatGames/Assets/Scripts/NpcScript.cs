c351210f777b2a3e04a2bcd0379464368a201564
MoaLindgren <moa.lindgren@outlook.com>
2019-03-09 12:28:25 +0100

    testing navmesh movement

---
+++ MakeGreatGames/Assets/Scripts/NpcScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcScript : TankScript
+{
+    NavMeshAgent agent;
+    float maxDistance;
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    bool move;
+    PlayerScript target;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        agent = GetComponent<NavMeshAgent>();
+        target = FindObjectOfType<PlayerScript>();
+    }
+
+    void Update()
+    {
+        if(move)
+        {
+            agent.Move(target.Position);
+        }
+    }
+
+}
---
+++ MakeGreatGames/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : TankScript
+{
+    Vector3 position;
+
+    public Vector3 Position
+    {
+        get { return position; }
+    }
+
+    private void Start()
+    {
+        position = transform.position;
+    }
+}
